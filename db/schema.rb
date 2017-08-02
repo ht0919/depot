fb32d709f2513d9e39d1d96d21cee3319b371566
Hiroyuki Tanaka <ht0919@gmail.com>
2017-08-02 21:14:54 +0900

    validation!

--- db/schema.rb
+++ db/schema.rb
@@ -18,8 +18,8 @@ ActiveRecord::Schema.define(:version => 20170712110944) do
     t.text     "description"
     t.string   "image_url"
     t.decimal  "price",       :precision => 8, :scale => 2
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                                :null => false
+    t.datetime "updated_at",                                :null => false
   end
 
 end
