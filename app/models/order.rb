a7f55ebe3343f9c2e691321afa16de81262faa73
Hiroyuki Tanaka <ht0919@gmail.com>
2017-10-18 21:00:06 +0900

    TASK G1 Finished

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,16 @@
 # coding: utf-8
 class Order < ActiveRecord::Base
+  has_many :line_items, dependent: :destroy
   #attr_accessible :address, :email, :name, :pay_type
-  PAYMENT_TYPES = [ "現金", "クレジットカード", "註文書" ]
+  PAYMENT_TYPES = [ "現金", "クレジットカード", "注文書" ]
+  # ...
+  validates :name, :address, :email, presence: true
+  validates :pay_type, inclusion: PAYMENT_TYPES
+  # ...
+  def add_line_item_from_cart(cart)
+    cart.line_items.each do |item|
+      item.cart_id = nil
+      line_items << item
+    end
+  end
 end
